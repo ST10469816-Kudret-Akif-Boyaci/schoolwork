1577585b6697d0345bc24be431d94e0975d6a05a
ST10469816-Kudret-Akif-Boyaci <st10469816@vcconnect.edu.za>
2025-03-06 13:59:59 +0200

    Add files via upload

---
+++ CarlysMotto/src/main/java/com/varsity/carlysmotto/CarlysMotto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.varsity.carlysmotto;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class CarlysMotto {
+
+    public static void main(String[] args) {
+    System.out.println("carly makes the food that makes it a party");
+    }
+}
---
+++ CarlysMotto/src/main/java/com/varsity/carlysmotto/CarlysMotto2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.varsity.carlysmotto;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class CarlysMotto2 {
+    public static void main(String[] args) {
+        
+        System.out.println("******************************************");
+        System.out.println("carly makes the food that makes it a party");
+        System.out.println("******************************************");
+    }
+}
